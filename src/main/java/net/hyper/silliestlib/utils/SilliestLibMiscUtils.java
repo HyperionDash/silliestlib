6554d4f9887159981a698b567dc3ecdf4ee2ddbb
HyperionDash <maxbatty404@gmail.com>
2026-08-31 23:56:54 +0100

    smol change

--- src/main/java/net/hyper/silliestlib/utils/SilliestLibMiscUtils.java
+++ src/main/java/net/hyper/silliestlib/utils/SilliestLibMiscUtils.java
@@ -4,23 +4,18 @@ import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.hyper.silliestlib.SilliestLib;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import static net.hyper.silliestlib.utils.SilliestLibRegUtils.getRK;
 
 public interface SilliestLibMiscUtils {
-    static void logIgniteAndBurnOdds(int igniteOdds, int burnOdds, Block block, Block baseBlock) {
-        SilliestLib.LOGGER.debug("igniteOdds("+igniteOdds+") and burnOdds("+burnOdds+") for "+getRK(block).identifier()+" successfully copied from "+getRK(baseBlock).identifier());
-    }
     static void addFlammableBlockFromExistingBlock(Block block, Block baseBlock) {
-        int igniteOdds = FlammableBlockRegistry.getDefaultInstance().get(baseBlock).getIgniteOdds();
-        int burnOdds = FlammableBlockRegistry.getDefaultInstance().get(baseBlock).getBurnOdds();
-        FlammableBlockRegistry.getDefaultInstance().add(block, igniteOdds, burnOdds);
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) logIgniteAndBurnOdds(igniteOdds, burnOdds, block, baseBlock);
+        addFlammableBlockFromExistingBlock(Blocks.FIRE, block, baseBlock);
     }
     static void addFlammableBlockFromExistingBlock(Block instance, Block block, Block baseBlock) {
         int igniteOdds = FlammableBlockRegistry.getInstance(instance).get(baseBlock).getIgniteOdds();
         int burnOdds = FlammableBlockRegistry.getInstance(instance).get(baseBlock).getBurnOdds();
         FlammableBlockRegistry.getDefaultInstance().add(block, igniteOdds, burnOdds);
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) logIgniteAndBurnOdds(igniteOdds, burnOdds, block, baseBlock);
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) SilliestLib.LOGGER.debug("igniteOdds("+igniteOdds+") and burnOdds("+burnOdds+") for "+getRK(block).identifier()+" successfully copied from "+getRK(baseBlock).identifier());
     }
 }
